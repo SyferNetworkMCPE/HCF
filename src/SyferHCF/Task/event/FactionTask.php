3d88afaa52b5f69be155bca169248a1c66cf4c5b
Hzpeed <86213937+Deepzzh@users.noreply.github.com>
2021-10-30 12:16:34 -0500

    Create FactionTask.php

---
+++ src/SyferHCF/Task/event/FactionTask.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SyferHCF\Task\event;
+
+use SyferHCF\{Loader, Factions};
+use SyferHCF\player\Player;
+
+use SyferHCF\AsyncTask\FactionData;
+
+use pocketmine\scheduler\Task;
+use pocketmine\utils\TextFormat as TE;
+
+class FactionTask extends Task {
+
+    /**
+     * FactionTask Constructor.
+     */
+    public function __construct(){
+        
+    }
+
+    /**
+     * @param Int $currentTick
+     * @return void
+     */
+    public function onRun(Int $currentTick) : void {
+    	$time = microtime(true);
+    	try {
+	    	foreach(Factions::getFactions() as $factionName){
+	    		if(!Factions::isFreezeTime($factionName)){
+	         	   Factions::setStrength($factionName, Factions::getMaxStrength($factionName));
+	      	  }
+	        }
+	        Loader::getInstance()->getLogger()->info("Backup of factions was completed in: ".round((microtime(true) - $time), 3)." seconds");
+		} catch(\Exception $exception){
+			Loader::getInstance()->getLogger()->info($exception->getMessage());
+		}
+    }
+}
+
+?>
