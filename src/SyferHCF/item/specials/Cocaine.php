47fe472b35971e0269e16e5ef8f443a5096b45ce
Hzpeed <86213937+Deepzzh@users.noreply.github.com>
2021-10-30 14:25:25 -0500

    Create Cocaine.php

---
+++ src/SyferHCF/item/specials/Cocaine.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SyferHCF\item\specials;
+
+use SyferHCF\Loader;
+use SyferHCF\player\Player;
+
+use pocketmine\utils\TextFormat as TE;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\item\enchantment\{Enchantment, EnchantmentInstance};
+
+class Cocaine extends Custom {
+	
+	const CUSTOM_ITEM = "CustomItem";
+	
+	/**
+	 * Cocaine Constructor.
+	 */
+	public function __construct(){
+		parent::__construct(self::SUGAR, "§l§aCocaina", [TE::GRAY."can get speed 4 for yourself.\n\n§aAvailable for purchase at §csyferhcf.tebex.io§a!"]);
+		$this->setNamedTagEntry(new CompoundTag(self::CUSTOM_ITEM));
+		$this->addEnchantment(new EnchantmentInstance(Enchantment::getEnchantment(Enchantment::UNBREAKING), 3));
+	}
+	
+	/**
+     * @return Int
+     */
+    public function getMaxStackSize() : Int {
+        return 64;
+    }
+}
+
+?>
