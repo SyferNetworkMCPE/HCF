d6e28c78fb3b7743a367d59a4c6b11a12f132993
Hzpeed <86213937+Deepzzh@users.noreply.github.com>
2021-10-30 14:44:29 -0500

    Create Strength.php

---
+++ src/SyferHCF/item/specials/Strength.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SyferHCF\item\specials;
+
+use SyferHCF\Loader;
+use SyferHCF\player\Player;
+
+use pocketmine\utils\TextFormat as TE;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\item\enchantment\{Enchantment, EnchantmentInstance};
+
+class Strength extends Custom {
+	
+	const CUSTOM_ITEM = "CustomItem";
+	
+	/**
+	 * Strength Constructor.
+	 */
+	public function __construct(){
+		parent::__construct(self::BLAZE_POWDER, "§l§aStrength", [TE::GRAY."when you use it you receive strength 2 for 15 seconds\n\n§aAvailable for purchase at §csyferhcf.tebex.io§a!"]);
+		$this->setNamedTagEntry(new CompoundTag(self::CUSTOM_ITEM));
+		$this->addEnchantment(new EnchantmentInstance(Enchantment::getEnchantment(Enchantment::UNBREAKING), 3));
+	}
+	
+	/**
+     * @return Int
+     */
+    public function getMaxStackSize() : Int {
+        return 64;
+    }
+}
+
+?>
