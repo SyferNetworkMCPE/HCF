66a74cb04e52209dde6e50f3c29339293e62cdde
Hzpeed <86213937+Deepzzh@users.noreply.github.com>
2021-10-30 13:21:13 -0500

    Create WindowIds.php

---
+++ src/SyferHCF/network/WindowIds.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SyferHCF\network;
+
+interface WindowIds {
+
+    public const ANVIL = 2008;
+
+    public const ENCHANT = 2009;
+
+    public const BEACON = 1981;
+    
+    public const HOPPER = 2021;
+    
+    public const BREWING_STAND = 2022;
+}
