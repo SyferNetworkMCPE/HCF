d44719a50ccf1a884e75ce659b9207b82ac4ff08
Hzpeed <86213937+Deepzzh@users.noreply.github.com>
2021-10-30 13:27:40 -0500

    Create KEYALL.php

---
+++ src/SyferHCF/listeners/event/KEYALL.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace SyferHCF\listeners\event;
+
+use SyferHCF\Loader;
+use SyferHCF\player\Player;
+use SyferHCF\utils\Time;
+
+use SyferHCF\Task\event\KEYALLTask;
+
+use pocketmine\event\Listener;
+
+class KEYALL implements Listener {
+	
+	/** @var Loader */
+	protected $plugin;
+	
+	/** @var bool */
+	protected static $enable = false;
+	
+	/** @var Int */
+	protected static $time = 0;
+	
+	/**
+	 * KEYALL Constructor.
+	 * @param Loader $plugin
+	 */
+	public function KEYALL__construct(Loader $plugin){
+		$this->plugin = $plugin;
+	}
+	
+	/**
+	 * @return bool
+	 */
+	public static function isEnable() : bool {
+		return self::$enable;
+	}
+	
+	/**
+	 * @param bool $enable
+	 */
+	public static function setEnable(bool $enable){
+		self::$enable = $enable;
+	}
+	
+	/**
+	 * @param Int $time
+	 */
+	public static function setTime(Int $time){
+		self::$time = $time;
+	}
+	
+	/**
+	 * @return Int
+	 */
+	public static function getTime() : Int {
+		return self::$time;
+	}
+	
+	/**
+	 * @return void
+	 */
+	public static function start(Int $time = 60) : void {
+		self::setEnable(true);
+		Loader::getInstance()->getScheduler()->scheduleRepeatingTask(new KEYALLTask($time), 20);
+	}
+	
+	/**
+	 * @return void
+	 */
+	public static function stop() : void {
+		self::setEnable(false);
+	}
+}
+
+?>
