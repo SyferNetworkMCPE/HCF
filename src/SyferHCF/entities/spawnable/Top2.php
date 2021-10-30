6f35ca05fe6fbe8ea9f23b109f44984592d6f28f
Hzpeed <86213937+Deepzzh@users.noreply.github.com>
2021-10-30 15:17:15 -0500

    Create Top2.php

---
+++ src/SyferHCF/entities/spawnable/Top2.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SyferHCF\entities\spawnable;
+
+use pocketmine\entity\Human;
+
+/**
+* Class Top2
+* @package SyferHCF\entities\spawnable
+*/
+class Top2 extends Human {
+
+  /**
+  * @return string
+  */
+  public function getName(): string {
+    return '';
+  }
+}
