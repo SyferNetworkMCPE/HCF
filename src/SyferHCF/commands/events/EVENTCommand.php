66c7c5220e1292eb43aa60329124cb3137352f53
Hzpeed <86213937+Deepzzh@users.noreply.github.com>
2021-10-30 22:10:44 -0500

    Create EVENTCommand.php

---
+++ src/SyferHCF/commands/events/EVENTCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace SyferHCF\commands\events;
+
+use SyferHCF\Loader;
+use SyferHCF\player\Player;
+
+use SyferHCF\listeners\event\EVENT;
+use SyferHCF\utils\Translator;
+
+use pocketmine\command\{CommandSender, PluginCommand};
+use pocketmine\utils\TextFormat as TE;
+
+class EVENTCommand extends PluginCommand {
+	
+	/**
+	 * EVENTCommand Constructor.
+	 */
+	public function __construct(){
+		parent::__construct("packageall", Loader::getInstance());
+		parent::setDescription("handle pp event");
+	}
+	
+	/**
+	 * @param CommandSender $sender
+	 * @param String $label
+	 * @param Array $args
+	 * @return void
+	 */
+	public function execute(CommandSender $sender, String $label, Array $args) : void {
+		if(count($args) === 0){
+			$sender->sendMessage(TE::RED."Use: /{$label} <on|off>");
+			return;
+		}
+		if(!$sender->isOp()){
+			$sender->sendMessage(TE::RED."You have not permissions to use this command");
+			return;
+		}
+		switch($args[0]){
+			case "on":
+				if(!$sender->isOp()){
+					$sender->sendMessage(TE::RED."You have not permissions to use this command");
+					return;
+				}
+				if(empty($args[1])){
+					$sender->sendMessage(TE::RED."Use: /{$label} {$args[0]} [Int: time]");
+					return;
+				}
+				if(EVENT::isEnable()){
+					$sender->sendMessage(TE::RED."The event was started before, you can't do this!");
+					return;
+				}
+				EVENT::start($args[1]);
+			break;
+			case "off":
+				if(!$sender->isOp()){
+					$sender->sendMessage(TE::RED."You have not permissions to use this command");
+					return;
+				}
+				if(!EVENT::isEnable()){
+					$sender->sendMessage(TE::RED."The event was never started, you can't do this!");
+					return;
+				}
+				EVENT::stop();
+			break;
+		}
+	}
+}
+
+?>
